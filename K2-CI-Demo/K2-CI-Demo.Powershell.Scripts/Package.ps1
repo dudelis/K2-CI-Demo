c2a4ae9a6e38b6b379307caaa7d191d519a63f58
dudelis <fukszon.kostya@gmail.com>
2018-02-28 11:57:31 +0100

    Fixed the deployment script

--- K2-CI-Demo/K2-CI-Demo.Powershell.Scripts/Package.ps1
+++ K2-CI-Demo/K2-CI-Demo.Powershell.Scripts/Package.ps1
@@ -1,3 +1,19 @@
 ﻿#
 # Script.ps1
 #
+
+param([string]$appName, [string]$categoryPath)
+
+if(-not($appName)){ Throw "You must provide the name for your application"}
+if(-not($categoryPath)){ Throw "You must provide the path for your application category"}
+
+#01. Create a folder for the output
+New-Item -ItemType Directory -Force -Path $PSScriptRoot\PND
+
+#02. Create a package
+Import-Module $PSScriptRoot\Assemblies\K2Field.Powershell.Module.dll -Verbose -Force
+New-K2Package -Path "$PSScriptRoot\PND\$appName.kspx" -Category $categoryPath
+
+#03. Create a Deployment config file
+Add-PSSnapin SourceCode.Deployment.Powershell 
+Write-DeploymentConfig "$PSScriptRoot\PND\$appName.kspx" "$PSScriptRoot\PND\$appName.xml"
